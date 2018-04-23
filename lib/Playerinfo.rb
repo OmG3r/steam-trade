34bd4216d1dfc9c67eed4b0e2e352dc1aa34f58d
OmG3r <adam.boulila@live.fr>
2018-04-23 14:28:02 +0100

    Added the ability to get player information

---
+++ lib/Playerinfo.rb
@@ -0,0 +1,28 @@
+module PlayerCommands
+
+      def get_player_summaries(*steamids)
+            if steamids.length == 0
+                  puts "no steamids supplied"
+                  return {'success' => false}
+            end
+            if @api_key == nil
+                  puts "no api_key loaded"
+                  return {'success' => false}
+            end
+            write = steamids.join(',')
+            html = @session.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=#{@api_key}&steamids=#{write}").content
+            return JSON.parse(html)["response"]["players"]
+      end
+
+      def get_friends(steamid)
+            if @api_key == nil
+                  puts "no api_key loaded"
+                  return {'success' => false}
+            end
+            html = @session.get("http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=#{@api_key}&steamid=#{@steamid}&relationship=friend").content
+            return JSON.parse(html)["friendslist"]["friends"]
+      end
+
+
+
+end
