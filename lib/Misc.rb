01deb26a8d1a380eff1554608d735effd17453c1
OmG3r <adam.boulila@live.fr>
2018-06-09 12:20:06 +0100

    added class methods

--- lib/Misc.rb
+++ lib/Misc.rb
@@ -34,7 +34,7 @@ module MiscCommands
 
 
 
-      private
+
       def partner_id_to_steam_id(account_id)
             unknown_constant = 17825793 # or 0x1100001 idk wtf is this but ....
             first_bytes = [account_id.to_i].pack('i>')
@@ -43,10 +43,11 @@ module MiscCommands
             return collect.unpack('Q>')[0].to_s
       end
 
+
       def output(message)
             time = Time.new
             add = time.strftime("%d-%m-%Y %H:%M:%S")
-            puts "#{add} :: #{message}" if @messages == true
+            puts "#{add} :: #{message}" if message != ''
       end
 
       def verify_profileid_or_trade_link_or_steamid(steamid)
@@ -74,22 +75,18 @@ module MiscCommands
             begin
                   value = @session.cookie_jar.jar["steamcommunity.com"]["/"]["sessionid"].value
             rescue
-                  value = nil
+                  @session.get('http://steamcommunity.com')
+                  value = @session.cookie_jar.jar["steamcommunity.com"]["/"]["sessionid"].value
             end
-            if value == nil
-                  begin
-                        value = @session.cookie_jar.jar["store.steampowered.com"]["/"]["sessionid"].value
-                  rescue
-                        value = nil
-                  end
-            end
+            return value
+      end
 
-            if value == nil
-                  @session.cookies.each { |c|
-                        if c.name == "sessionid"
-                               value = c.value
-                         end
-                  }
+      def store_cookie()
+            begin
+                  value = @session.cookie_jar.jar["store.steampowered.com"]["/"]["sessionid"].value
+            rescue
+                  @session.get('http://store.steampowered.com')
+                  value = @session.cookie_jar.jar["store.steampowered.com"]["/"]["sessionid"].value
             end
             return value
       end
@@ -108,6 +105,52 @@ module MiscCommands
             end
             return response.content
       end
+
+
+
+
+      def self.included(base)
+           base.extend(Misc_ClassMethods)
+      end
+
+      module Misc_ClassMethods
+            def partner_id_to_steam_id(account_id)
+                  unknown_constant = 17825793 # or 0x1100001 idk wtf is this but ....
+                  first_bytes = [account_id.to_i].pack('i>')
+                  last_bytes = [unknown_constant].pack('i>')
+                  collect = last_bytes + first_bytes
+                  return collect.unpack('Q>')[0].to_s
+            end
+
+            private
+            def output(message)
+                  time = Time.new
+                  add = time.strftime("%d-%m-%Y %H:%M:%S")
+                  puts "#{add} :: #{message}"
+            end
+
+            def verify_profileid_or_trade_link_or_steamid(steamid)
+                  if steamid.to_i == 0 && steamid.include?("?partner=") ##supplied trade link
+                      partner_raw = steamid.split('partner=',2)[1].split('&',2)[0]
+                      token = steamid.split('token=', 2)[1]
+                      steamid = partner_id_to_steam_id(partner_raw)
+                      return [steamid,token]
+                  elsif steamid.to_i == 0
+                        session = Mechanize.new
+                        parser = Nokogiri::XML(session.get("https://steamcommunity.com/id/#{steamid}?xml=1").content)
+                        if parser.xpath('//error').text == ('The specified profile could not be found.')
+                              raise "No profile with #{steamid} as profileid"
+                        end
+                        steamid = parser.xpath('//steamID64').text
+                        return steamid
+                  elsif steamid.to_s.length == 17
+                        return steamid
+                  else
+                        raise "invalid steamid : #{steamid}, length of received :: #{steamid.to_s.length}, normal is 17" if steamid.to_s.length != 17
+                  end
+            end
+
+      end ## end module
 
 
 end
