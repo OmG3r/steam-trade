74f8ffc38849479d66179f0981264703a07fff71
OmG3r <adam.boulila@live.fr>
2018-06-09 12:19:52 +0100

    fixed issues with ruby 2.4+

--- lib/LoginExecutor.rb
+++ lib/LoginExecutor.rb
@@ -8,45 +8,79 @@ module LoginCommands
             encrypted_password = data["password"]
             timestamp = data["timestamp"]
             repeater = 0
-            until repeater == true
-                  if @secret != nil
-                        guardcode = fa(@secret,@time_difference)
-                  else
-                        puts "please write your 2FA code"
-                        guardcode = gets.chomp
-                  end
 
+
+            send = {
+                  'password' => encrypted_password,
+                  'username' => @username,
+                  'twofactorcode' =>'', #update
+                  'emailauth' => '',
+                  'loginfriendlyname' => '',
+                  'captchagid' => '-1',
+                  'captcha_text' => '',
+                  'emailsteamid' => '',
+                  'rsatimestamp' => timestamp,
+                  'remember_login' => @remember
+            }
+            login = @session.post('https://store.steampowered.com/login/dologin', send ).content
+            firstreq = JSON.parse(login)
+
+            raise "Incorrect username or password" if firstreq["message"] == "The account name or password that you have entered is incorrect."
+
+
+            until firstreq["success"] == true
+                  sleep(0.3)
+                  gid = '-1'
+                  cap = ''
+                  if firstreq['captcha_needed'] == true
+                        gid = firstreq['captcha_needed']
+                        File.delete("./#{username}_captcha.png") if File.exist?("./#{username}_captcha.png")
+                        @session.get("https://store.steampowered.com/login/rendercaptcha?gid=#{gid}").save "./#{@username}_captcha.png"
+                        puts "you need to write a captcha to continue"
+                        puts "there is an image named #{@username}_captcha in the script directory"
+                        puts "open it and write the captha here"
+                        cap = gets.chomp
+                  end
+                  emailauth = ''
+                  facode = ''
+                  emailsteamid = ''
+                  if firstreq['requires_twofactor'] == true
+                        if @secret.nil?
+                              puts "write 2FA code"
+                              facode = gets.chomp
+                        else
+                              facode = fa(@secret,@time_difference)
+                        end
+                  elsif firstreq['emailauth_needed'] == true
+                        emailsteamid = firstreq['emailsteamid']
+                        puts "Guard code was sent to your email"
+                        puts "write the code"
+                        emailauth = gets.chomp
+                  end
 
                   send = {
                         'password' => encrypted_password,
                         'username' => @username,
-                        'twofactorcode' =>guardcode, #update
-                        'emailauth' => '',
+                        'twofactorcode' => facode, #update
+                        'emailauth' => emailauth,
                         'loginfriendlyname' => '',
-                        'captchagid' => '-1',
-                        'captcha_text' => '',
-                        'emailsteamid' => '',
+                        'captchagid' => gid,
+                        'captcha_text' => cap,
+                        'emailsteamid' => emailsteamid,
                         'rsatimestamp' => timestamp,
                         'remember_login' => @remember
                   }
-
-                  login = @session.post('https://store.steampowered.com/login/dologin', send )
-                  response = JSON::parse(login.body)
-                  output "logging-in"
-                  if response["success"] == true
-                        repeater = true
-                  elsif repeater == 3
-                        raise "Login failed username: #{@username}, password: #{@password}, shared_scret: #{@secret} tried 3 times"
-                 else
-                       print response
-                       puts "re-trying to login"
-                       puts "sleeping for 6 seconds"
-                       sleep(6)
-                       repeater = repeater + 1
-                 end
-
+                  output "attempting to login"
+                  login = @session.post('https://store.steampowered.com/login/dologin', send ).content
+                  firstreq = JSON.parse(login)
 
             end
+            response = firstreq
+
+
+
+
+
             if @steamid != nil && @steamid != response["transfer_parameters"]["steamid"]
                   puts "the steamid you provided does not belong to the account you entered"
                   puts "steamid will be overwritten"
@@ -73,8 +107,11 @@ module LoginCommands
             cookie = Mechanize::Cookie.new :domain => 'steamcommunity.com', :name =>'sessionid', :value =>steampowered_sessionid, :path => '/'
             @session.cookie_jar << cookie
             @loggedin = true
-            @api_key = Nokogiri::HTML(@session.get("https://steamcommunity.com/dev/apikey").content).css('#bodyContents_ex').css('p').first.text.sub('Key: ','')
-
+            begin
+                  @api_key = Nokogiri::HTML(@session.get("https://steamcommunity.com/dev/apikey").content).css('#bodyContents_ex').css('p').first.text.sub('Key: ','')
+            rescue
+                  output "Could not retrieve api_key"
+            end
             data = get_player_summaries(@steamid)
             data.each { |element|
                   if element["steamid"].to_s == @steamid.to_s
@@ -83,7 +120,7 @@ module LoginCommands
             }
             output "logged in as #{@persona}"
             output "your steamid is #{@steamid}"
-            output "loaded API_KEY : #{@api_key}"
+            output "loaded API_KEY : #{@api_key}" if !@api_key.nil?
       end
 ########################################################################################
 
@@ -97,8 +134,13 @@ module LoginCommands
             timestamp = data["timestamp"]
 
             key   = OpenSSL::PKey::RSA.new
-            key.e = OpenSSL::BN.new(exp)
-            key.n = OpenSSL::BN.new(mod)
+            if RUBY_VERSION.to_f <= 2.3
+                  key.e = OpenSSL::BN.new(exp)
+                  key.n = OpenSSL::BN.new(mod)
+            elsif RUBY_VERSION.to_f >= 2.4
+                  #key.set_key(n, e, d)
+                  key.set_key(OpenSSL::BN.new(mod), OpenSSL::BN.new(exp),nil)
+            end
             ep = Base64.encode64(key.public_encrypt(password.force_encoding("utf-8"))).gsub("\n", '')
             return {'password' => ep, 'timestamp' => timestamp }
       end
