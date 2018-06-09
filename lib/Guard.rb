61a74bc08e40b928cba4e061bff1602be74fb5d2
OmG3r <adam.boulila@live.fr>
2018-06-09 12:19:06 +0100

    added class methods

--- lib/Guard.rb
+++ lib/Guard.rb
@@ -30,6 +30,40 @@ module GuardCommands
 
       end
 
+      def self.included(base)
+           base.extend(Guard_ClassMethods)
+     end
+
+      module Guard_ClassMethods
+            def fa(shared_secret,time_difference = 0)
+                  raise "No shared_secret given" if shared_secret == nil # cause upon initialization @secret = nil
+                  timestamp = Time.new.to_i + time_difference
+                  math = timestamp.to_i / 30
+                  math = math.to_i
+                  time_buffer =[math].pack('Q>')
+
+                  hmac = OpenSSL::HMAC.digest('sha1', Base64.decode64(shared_secret), time_buffer)
+
+                  start = hmac[19].ord & 0xf
+                  last = start + 4
+                  pre = hmac[start..last]
+                  fullcode = pre.unpack('I>')[0] & 0x7fffffff
+
+                  chars = '23456789BCDFGHJKMNPQRTVWXY'
+                  code= ''
+                  for looper in 0..4 do
+                        copy = fullcode #divmod
+                        i = copy % chars.length #divmod
+                        fullcode = copy / chars.length #divmod
+                        code = code + chars[i]
+                  end
+                  return code
+
+            end
+
+
+      end
+
       private
       def generate_confirmation_key(tag_string, time_stamp)
             buffer = [time_stamp].pack('Q>') + tag_string.encode('ascii')
