279a443eec97f91bd1c801fa100f9d60be2f1643
OmG3r <adam.boulila@live.fr>
2018-06-23 04:00:54 +0100

    salien_card()

--- lib/EventCards.rb
+++ lib/EventCards.rb
@@ -52,12 +52,106 @@ module EventCommands
 
            total = @username + " :: " + h3
            done  = @username + " :: " + subtext
-           
+
            return {'total' => total, 'done' => done}
 
       end
 
+      def salien_card()
+            raise "must be logged in to use the command" if @logged == false
+            @saliened = [] if @saliened.nil?
 
+
+            data = @session.get('https://steamcommunity.com/saliengame/gettoken')
+            data = JSON.parse(data.content)
+            game_token = data['token']
+
+
+            headers = {
+                  'origin' => 'https://steamcommunity.com',
+                  'referer' => 'https://steamcommunity.com/saliengame/play/'
+            }
+
+            params = {
+                  'access_token' => game_token
+            }
+
+            data = @session.post('https://community.steam-api.com/ITerritoryControlMinigameService/GetPlayerInfo/v0001/', params, headers)
+            data = JSON.parse(data.content)['response']
+            planet = data['active_planet']
+            active_game = data["active_zone_game"]
+
+            if planet.nil?
+                  data = @session.get('https://community.steam-api.com/ITerritoryControlMinigameService/GetPlanets/v0001/?active_only=1&language=english')
+                  data = JSON.parse(data.content)['response']
+                  planet = data['planets'].first["id"]
+
+                  headers = {
+                        'origin' => 'https://steamcommunity.com',
+                        'referer' => 'https://steamcommunity.com/saliengame/play/'
+                  }
+
+                  params = {
+                        'id' => planet,
+                        'access_token' => game_token
+                  }
+
+
+                  @session.post('https://community.steam-api.com/ITerritoryControlMinigameService/JoinPlanet/v0001/', params, headers )
+            end
+
+
+            if active_game != nil
+                  headers = {
+                        'origin' => 'https://steamcommunity.com',
+                        'referer' => 'https://steamcommunity.com/saliengame/play/'
+                  }
+                  
+                  params = {
+                      'gameid' => active_game,
+                      'access_token' => game_token
+                  }
+                  @session.post('https://community.steam-api.com/IMiniGameService/LeaveGame/v0001/', params, headers)
+
+
+            end
+
+
+
+            data = @session.get("https://community.steam-api.com/ITerritoryControlMinigameService/GetPlanet/v0001/?id=#{planet}&language=english")
+            data = JSON.parse(data.content)['response']
+
+            to_play = nil
+            left  = []
+            data['planets'].first['zones'].each { |zone|
+                  if zone['captured'] == false && (@saliened.include?(zone['zone_position']) == false)
+                        left << zone['zone_position']
+
+                  end
+                  #position = zone['zone_position']
+                  #captured = zone['captured']
+            }
+            to_play = left[rand(left.length - 1)]
+            headers = {
+                  'Origin' => 'https://steamcommunity.com',
+                  'Referer' => 'https://steamcommunity.com/saliengame/play/',
+                  'user-agent' => 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36 OPR/53.0.2907.99'
+            }
+
+            params = {
+                  'zone_position' => to_play,
+                  'access_token' => game_token
+            }
+
+
+            @session.post('https://community.steam-api.com/ITerritoryControlMinigameService/JoinZone/v0001/', params, headers)
+            sleep(0.3)
+            @saliened << to_play
+            output "collected 1 salien game card"
+
+
+
+      end
 
 
 end
